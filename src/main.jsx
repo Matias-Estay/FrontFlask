7a547064c275a90bf0518b1ab4513cdb1739b0bb
Matías Note <mestay@jri.cl>
2023-12-14 22:40:26 -0300

    general import .env

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import {RouterProvider,} from "react-router-dom"
 import './index.css'
 import routes from './routes/router.jsx'
 import axios from 'axios'
-require('dotenv').config()
-axios.defaults.baseURL = process.env.API_URL
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <RouterProvider router={routes} />
